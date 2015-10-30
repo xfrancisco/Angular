3009f7c3bfa8950f8ca848df6deffc3b0fd7c227
xfrancisco <xavier.francisco@canal-plus.com>
2015-10-31 00:22:28 +0100

    Nettoyage, Users + token node

---
+++ NodeJS/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://localhost:27017/xf81'
+    // 'mongodb://webuser:@ds041154.mongolab.com:41154/xf81'
+};
--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,104 +1,191 @@
-//  server.js
-//  https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
-//  mongod.exe --dbpath ..\data
+// Packages
+var express    = require('express'); // Framework node
+var app        = express();
+var bodyParser = require('body-parser'); // Accès aux flux des request
+var mongoose   = require('mongoose'); // Framework mongo
+var uriUtil = require('mongodb-uri'); // Utile pour formatter les URL mongo
+var jwt    = require('jsonwebtoken'); // Création, signature et contrôle des tokens
+var morgan      = require('morgan'); // Log des request dans la console
 
-// BASE SETUP
-// =============================================================================
+// Fichier de configuration
+var config = require('./config'); // get our config file
 
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+// Modèle de données
+var Account = require('./app/models/account');
+var User   = require('./app/models/user'); 
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// Configuration du body parser (accès aux paramètres et à l'url)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+// Port du serveur
+var port = process.env.PORT || 8080;
 
-var Comptes     = require('./app/models/comptes');
-
-var mongoose   = require('mongoose');
-var uriUtil = require('mongodb-uri');
+// Options
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } };
 
-//var args = process.argv.slice(2);
+// Parsing des paramètres du script
 var isInit = false;
 var isOnline = false;
 process.argv.forEach(function (val, index, array) {
     if (val == 'init')
         isInit = true;
-    if (val == 'online')
-        isOnline = true;
 });
 
-var dbURI = '';
-if (isOnline)
-    dbUri = 'mongodb://webuser:@ds041154.mongolab.com:41154/xf81';
-else
-    dbURI = 'mongodb://localhost:27017/xf81';
+// Formattage de l'url de mongo
+var mongooseUri = uriUtil.formatMongoose(config.database);
 
-var mongooseUri = uriUtil.formatMongoose(dbURI);
-
-
-
+// Connection à la base de données
 mongoose.connect(mongooseUri, function(err) {
     if (err) 
         throw err;
-}); // connect to our database
+});
 
+// Log des request dans la console
+app.use(morgan('dev'));
+
+
+
+
+
+// =============================================================================
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+
+// get an instance of the express Router
+var router = express.Router();              
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });    
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;    
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token provided.' 
+        });
+    }
 });
 
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: "Bienvenue sur l'API comptes!" });   
+    res.json({ message: "Bienvenue sur l'API Account!" });   
 });
 
-// more routes for our API will happen here
 
-// on routes that end in /comptes
-// ----------------------------------------------------
-var comptes = router.route('/comptes');
+// =============================================================================
+// USERS
+// =============================================================================
+router.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
 
-comptes.post(function(req, res) {
-        var compte = new Comptes();      
-        compte.officialAmount = parseFloat(req.body.officialAmount) * 100;
-        compte.unofficialAmount = parseFloat(req.body.unofficialAmount) * 100;
-        compte.dueDate = req.body.dueDate;
+router.post('/users', function(req, res) {
+    var user = new User();      
+    user.cuser = req.body.username;
+    user.pass = req.body.password;
+    user.indadmin = req.body.isAdmin;
+
+    user.save(function(err) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'User created!' });
+    });
+
+});
+
+// =============================================================================
+// AUTHENTICATE
+// =============================================================================
+
+router.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+    }
+  });
+});
+
+
+// =============================================================================
+// ACCOUNT
+// =============================================================================
+
+var account = router.route('/account');
+
+account.post(function(req, res) {
+        var account = new Account();      
+        account.officialAmount = parseFloat(req.body.officialAmount) * 100;
+        account.unofficialAmount = parseFloat(req.body.unofficialAmount) * 100;
+        account.dueDate = req.body.dueDate;
         
-        compte.save(function(err) {
+        account.save(function(err) {
             if (err)
                 res.send(err);
 
-            res.json({ message: 'Comptes created!' });
+            res.json({ message: 'Account created!' });
         });
         
     });
 
-comptes.get(function(req, res) {
-        Comptes.find({}).sort('dueDate').exec(function(err, comptes) {
+account.get(function(req, res) {
+        Account.find({}).sort('dueDate').exec(function(err, account) {
             if (err)
                 res.send(err);
 
-            res.json(comptes);
+            res.json(account);
         });
     });
 
     
-comptes.delete(function(req, res) {
-        Comptes.remove({}, function(err, comptes) {
+account.delete(function(req, res) {
+        account.remove({}, function(err, account) {
             if (err)
                 res.send(err);
 
@@ -107,55 +194,55 @@ comptes.delete(function(req, res) {
     });
 
 
-var comptesByDate = router.route('/comptes/:compteId');
+var accountByDate = router.route('/account/:accountId');
 
-comptesByDate.get(function(req, res) {
-        Comptes.findById(req.params.compteId, function(err, compte) {
+accountByDate.get(function(req, res) {
+        Account.findById(req.params.accountId, function(err, account) {
             if (err)
                 res.send(err);
-            res.json(compte);
+            res.json(account);
         });
     });
 
-comptesByDate.put(function(req, res) {
-    Comptes.findById(req.params.compteId, function(err, compte) {
+accountByDate.put(function(req, res) {
+    Account.findById(req.params.accountId, function(err, account) {
             if (err)
                 res.send(err);
-            compte.officialPaid = req.body.officialPaid;
-            compte.unofficialPaid = req.body.unofficialPaid;
+            account.officialPaid = req.body.officialPaid;
+            account.unofficialPaid = req.body.unofficialPaid;
             
-            if (compte.officialPaid) {
-                compte.officialPaidDate = new Date();
+            if (account.officialPaid) {
+                account.officialPaidDate = new Date();
             }
             else {
-                 compte.officialPaidDate = null;
+                 account.officialPaidDate = null;
             }
             
-            if (compte.unofficialPaid) {
-                compte.unofficialPaidDate = new Date();
+            if (account.unofficialPaid) {
+                account.unofficialPaidDate = new Date();
             }
             else {
-                compte.unofficialPaidDate = null;
+                account.unofficialPaidDate = null;
             }
             
-            compte.save(function(err) {
+            account.save(function(err) {
                 if (err){
                     res.json({ result : 'ERROR', message: err });
                 }
 
-                res.json({ result : 'SUCCESS', message: 'Compte mis à jour' });
+                res.json({ result : 'SUCCESS', message: 'Account updated' });
             });
 
         });
     });
 
 
-var allComptes = router.route('/statistics');
+var allAccounts = router.route('/statistics');
 
-allComptes.get(function(req, res) {
+allAccounts.get(function(req, res) {
     var result2 = new Object();
     
-    Comptes.aggregate(
+    Account.aggregate(
         { $group: {
                 _id :   null,
                 totalOfficialAmount: {$sum: "$officialAmount"},
@@ -170,7 +257,7 @@ allComptes.get(function(req, res) {
             result2.totalOfficialAmount = result[0].totalOfficialAmount;
             result2.totalUnofficialAmount = result[0].totalUnofficialAmount;
             result2.totalAmount = result2.totalOfficialAmount + result2.totalUnofficialAmount;
-            Comptes.aggregate(
+            Account.aggregate(
                 {
                     $match: {officialPaidDate:{$ne:null}}
                 },
@@ -189,7 +276,7 @@ allComptes.get(function(req, res) {
                         result2.totalPaidOfficialAmount = result[0].totalPaidOfficialAmount;
                     }
                     
-                    Comptes.aggregate(
+                    Account.aggregate(
                     {
                         $match: {unofficialPaidDate:{$ne:null}}
                     },
@@ -216,72 +303,31 @@ allComptes.get(function(req, res) {
     return;
 });
 
-/*
-// on routes that end in /bears/:bear_id
-// ----------------------------------------------------
-router.route('/bears/:bear_id')
-    // get the bear with that id (accessed at GET api/bears/:bear_id)
-    .get(function(req, res) {
-        Bear.findById(req.params.bear_id, function(err, bear) {
-            if (err)
-                res.send(err);
-            res.json(bear);
-        });
-    })
-    
-    // update the bear with this id (accessed at PUT api/bears/:bear_id)
-    .put(function(req, res) {
-        // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function(err, bear) {
-            if (err)
-                res.send(err);
-            bear.name = req.body.name;  // update the bears info
-            // save the bear
-            bear.save(function(err) {
-                if (err)
-                    res.send(err);
-
-                res.json({ message: 'Bear updated!' });
-            });
-
-        });
-    })
-
-    // delete the bear with this id (accessed at DELETE api/bears/:bear_id)
-    .delete(function(req, res) {
-        Bear.remove({
-            _id: req.params.bear_id
-        }, function(err, bear) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Successfully deleted' });
-        });
-    });
-*/
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
+
+// Initialisation de la base de données
 if (isInit) {
-    console.log('**************Initialisation BDD****************');
-    Comptes.remove({}, function(err, comptes) {
+    console.log('**************Initialize Database****************');
+    Account.remove({}, function(err, account) {
         if (err) {
-            console.log('Erreur lors de la suppression');
+            console.log('Error during initialization');
             exit;
         }
         var startDate = new Date("October 05, 2015 00:00:00");
         var maxDate = new Date("October 05, 2030 00:00:00");
         var cpt = 1;
         while (cpt < 182) {
-            var compte = new Comptes();
+            var account = new Account();
             var tmp = new Date(startDate);
-            compte.officialAmount = 500;
-            compte.unofficialAmount = 185;
-            compte.dueDate = tmp;
+            account.officialAmount = 500;
+            account.unofficialAmount = 185;
+            account.dueDate = tmp;
             cpt = cpt + 1;
 
-            compte.save(function(err) {
+            account.save(function(err) {
                 if (err){
                     console.log('Erreur sur insert');
                 }
